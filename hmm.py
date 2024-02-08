e55cbe76a49cb3d083fb3dcf4412994ef72558df
natekimball <natekimball1@gmail.com>
2024-02-08 13:22:51 -0500

    fixed viterbi decoding

--- hmm.py
+++ hmm.py
@@ -30,6 +30,7 @@ def forward(T, M, Y, pi):
             alpha[i,t] = M[Y[t],i] * np.sum(alpha[:,t-1] * T[i])
     return alpha
 
+# filtering algorithm
 def filtering(T, M, Y, pi):
     alpha = forward(T, M, Y, pi)
     return (alpha / np.sum(alpha, axis=0)).T
@@ -46,23 +47,14 @@ def backward(T, M, Y, pi):
     joint_prob  = np.sum(pi * M[:,Y[0]] * beta[:,0])
     return beta, joint_prob
 
-# def calculate_joint_probability(T, M, Y, pi):
-#     beta_1 = backward(T, M, Y)
-#     P_Y1_to_Yt = np.sum(beta_1[:, 0] * pi * M[Y[0]-1])
-#     return P_Y1_to_Yt
-#     # or
-#     # alpha = forward(T, M, Y, pi)
-#     # return np.sum(alpha[:, -1])
-
 # smoothing algorithm
 def smoothing(T, M, Y, pi):
     alpha = forward(T, M, Y, pi)
     beta, _ = backward(T, M, Y, pi)
-    print("alpha", alpha.T)
-    print("beta", beta.T)
+    # print("alpha", alpha.T)
+    # print("beta", beta.T)
     ab = alpha * beta
-    print("alpha * beta", ab.T)
-    # return (ab / np.sum(alpha[:, -1])).T
+    # print("alpha * beta", ab.T)
     return (ab / np.sum(ab, axis=0)).T
 
 # viterbi decoding algorithm
@@ -70,19 +62,19 @@ def viterbi(T, M, Y, pi):
     N = len(T)
     T = T.T
     M = M.T
-    delta = np.zeros((N, len(Y)))
     psi = np.zeros((N, len(Y)))
-    delta[:,0] = pi * M[Y[0]-1]
+    parents = np.zeros((N, len(Y)))
+    psi[:,0] = pi * M[Y[0]]
     for t in range(1, len(Y)):
         for i in range(N):
-            delta[i,t] = M[Y[t]-1,i] * np.max(delta[:,t-1] * T[i])
-            psi[i,t] = np.argmax(delta[:,t-1] * T[i])
+            psi[i,t] = M[Y[t],i] * np.max(psi[:,t-1] * T[i])
+            parents[i,t] = np.argmax(psi[:,t-1] * T[i])
+    # print("psi: \n", psi)
     path = np.zeros(len(Y))
-    path[-1] = np.argmax(delta[:,-1])
+    path[-1] = np.argmax(psi[:,-1])
     for t in range(len(Y)-2, -1, -1):
-        path[t] = psi[int(path[t+1]),t+1]
+        path[t] = parents[int(path[t+1]),t+1]
     return path
-
 
 T = np.array([
     [0, .5, .5],
@@ -97,6 +89,8 @@ M = np.array([
 pi = np.array([1, 0, 0])
 Y = np.array([2, 3, 3, 2, 2, 2, 3, 2, 3]) - 2 
 filter = filtering(T, M, Y, pi)
-print("filter", filter)
+print("filtering: \n", filter)
 smooth = smoothing(T, M, Y, pi)
-print("smooth", smooth)
+print("smoothing: \n", smooth)
+path = viterbi(T, M, Y, pi)
+print("decoded path: \n", path)
